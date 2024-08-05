566a81b6648e9c248786c121da65e3a452f09a2a
MinaHany256 <minahanyfoad@gmail.com>
2024-08-05 21:29:36 +0300

    Question 01

---
+++ Assignment Part 01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Part_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Assignment Part 02/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment_Part_02
+{
+    internal class Program
+    {
+        #region 1. Given an array of integers, count the frequency of each element using a hash table.
+
+        //public static Dictionary<int, int> CountFrequency(int[] Arr)
+        //{
+        //    Dictionary<int, int> FrequencyDictionary = new Dictionary<int, int>();
+
+        //    foreach (int i in Arr)
+        //    {
+        //        if (FrequencyDictionary.ContainsKey(i))
+        //            FrequencyDictionary[i]++;
+        //        else
+        //            FrequencyDictionary[i] = 1;
+        //    }
+        //    return FrequencyDictionary;
+        //} 
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region 1. Given an array of integers, count the frequency of each element using a hash table.
+
+            //int[] Numbers = { 1, 2, 2, 2, 3, 4, 5 };
+
+            //Dictionary<int, int> ResultDictionary = CountFrequency(Numbers);
+
+            //foreach (var i in ResultDictionary)
+            //    Console.WriteLine($" Key : {i.Key} ::: Count : {i.Value} ");
+
+            #endregion
+        }
+    }
+}
